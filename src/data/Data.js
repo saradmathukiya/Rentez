3dee7304c67db60bd574496f43b2294bafdeb860
Sharad Mathukiya <saradmathukiya9@gmail.com>
2024-02-06 22:26:58 +0530

    homepage 2nd section

---
+++ src/data/Data.js
@@ -0,0 +1,33 @@
+import asset1 from "../assets/asset 1.png";
+import asset2 from "../assets/asset 2.png";
+import asset3 from "../assets/asset 3.png";
+import asset4 from "../assets/asset 4.png";
+import asset5 from "../assets/asset 5.png";
+
+export const featured = [
+  {
+    cover: asset1,
+    name: "Family House",
+    total: "122 Property",
+  },
+  {
+    cover: asset2,
+    name: "House & Villa",
+    total: "155 Property",
+  },
+  {
+    cover: asset3,
+    name: "Apartment",
+    total: "300 Property",
+  },
+  {
+    cover: asset4,
+    name: "Office & Studio",
+    total: "80 Property",
+  },
+  {
+    cover: asset5,
+    name: "Villa & Condo",
+    total: "80 Property",
+  },
+];
